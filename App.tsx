92e7520bf713f13d34e4143fdf33ad386795bb94
Japjyot Suri <japjyot.suri@geekyants.com>
2024-06-21 17:22:22 +0530

    Authentication Done

--- App.tsx
+++ App.tsx
@@ -6,20 +6,40 @@ import {NavigationContainer} from '@react-navigation/native'
 import Signup from './Components/Signup'
 import Login from './Components/Login'
 import Homescreen from './Components/Homescreen'
-import { Text, View } from 'react-native'
+import { Alert, Pressable, SafeAreaView, Text, TextInput, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 const App = () => {
-   
+  const Stack=createNativeStackNavigator();
+//   const [email,setEmail]=useState('');
+//   const [password,setPassword]=useState('')
+//   function signUpFunc(){
+//       auth().createUserWithEmailAndPassword(email,password).then(()=> {
+//         Alert.alert("user created" + email,password)
+//         setEmail("");
+//         setPassword("");
+       
+//   }).catch((error) =>{
+//     console.log(error)
+//     Alert.alert(error.nativeErrorMessage)
+//   }
+// ) }
   return (
-    <View>
-      <Text>App</Text>
-      </View>
-    // <NavigationContainer>
-    // <Stack.Navigator initialRouteName='Login'>
-    //   <Stack.Screen name='Signup' component={Signup}></Stack.Screen>
-    //   <Stack.Screen name='Login' component={Login}></Stack.Screen>
-    //   <Stack.Screen name='Home' component={Homescreen}></Stack.Screen>
-    // </Stack.Navigator>
-    // </NavigationContainer>
+    // <SafeAreaView>
+    // <View>
+    //  <View>
+    //   <TextInput placeholder='Email' value={email} onChangeText={setEmail}/>
+    //   <TextInput placeholder='Password' value={password} onChangeText={setPassword}/>
+    //   <Pressable onPress={signUpFunc}><Text>Sign up</Text></Pressable>
+    // </View>
+    //   </View>
+    //   </SafeAreaView>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name='Signup' component={Signup}></Stack.Screen>
+      <Stack.Screen name='Login' component={Login}></Stack.Screen>
+      <Stack.Screen name='Home' component={Homescreen}></Stack.Screen>
+    </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
