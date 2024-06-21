da7d2af4e1e0ad6fd53fb2e2e2b688bd41c0217f
Japjyot Suri <japjyot.suri@geekyants.com>
2024-06-21 16:24:26 +0530

    Firebase setup

--- App.tsx
+++ App.tsx
@@ -1,24 +1,28 @@
-import { Alert, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+
 import React, { useState } from 'react'
 import auth from '@react-native-firebase/auth'
 import {NavigationContainer} from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import Signup from './Components/Signup'
 import Login from './Components/Login'
 import Homescreen from './Components/Homescreen'
+import { Text, View } from 'react-native'
 const App = () => {
-   const Stack=createNativeStackNavigator();
+   
   return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName='Login'>
-      <Stack.Screen name='Signup' component={Signup}></Stack.Screen>
-      <Stack.Screen name='Login' component={Login}></Stack.Screen>
-      <Stack.Screen name='Home' component={Homescreen}></Stack.Screen>
-    </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>App</Text>
+      </View>
+    // <NavigationContainer>
+    // <Stack.Navigator initialRouteName='Login'>
+    //   <Stack.Screen name='Signup' component={Signup}></Stack.Screen>
+    //   <Stack.Screen name='Login' component={Login}></Stack.Screen>
+    //   <Stack.Screen name='Home' component={Homescreen}></Stack.Screen>
+    // </Stack.Navigator>
+    // </NavigationContainer>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+
